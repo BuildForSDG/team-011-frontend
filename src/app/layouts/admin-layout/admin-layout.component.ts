05cb9daf2e1978e080632b4c88081ecf38b504a8
Fortune <fortuneochi@gmail.com>
2020-05-27 14:09:05 +0100

    feat: added some styles to admin layout

--- src/app/layouts/admin-layout/admin-layout.component.ts
+++ src/app/layouts/admin-layout/admin-layout.component.ts
@@ -15,7 +15,7 @@ import * as $ from 'jquery';
 @Component({
   selector: 'app-admin-layout',
   templateUrl: './admin-layout.component.html',
-  styleUrls: ['./admin-layout.component.scss'],
+  styleUrls: ['./admin-layout.component.css'],
 })
 export class AdminLayoutComponent implements OnInit {
   private _router: Subscription;
